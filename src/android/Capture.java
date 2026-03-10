ac0975390a228981da7445b6365fa8d2b5dc41f2
Veggy Priyangka <a-a-je-veggy.priyangka@hitachienergy.com>
2026-03-10 15:12:22 +0800

    fix: remove READ_MEDIA_IMAGES/VIDEO permissions for Google Play compliance

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -82,7 +82,7 @@ public class Capture extends CordovaPlugin {
     private static final String[] storagePermissions;
     static {
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            storagePermissions = new String[]{ Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.READ_MEDIA_AUDIO };
+            storagePermissions = new String[]{ Manifest.permission.READ_MEDIA_AUDIO };
         } else {
             storagePermissions = new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO };
         }
